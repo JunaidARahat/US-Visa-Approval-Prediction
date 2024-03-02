566cbc0716c9e9ee6a33e1060a8833b44410a4ff
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-03-02 18:04:16 +0500

    exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
 
-logging.info("Hello this is my first custom log")
+
+try:
+    a=1/"10"
+except Exception as e:
+    raise USvisaException(e, sys) from e
